679008848bd77f92adfe5350ec0f8f002c958e63
khaled saied <khaledsaied080@gmail.com>
2025-03-23 23:58:40 +0200

    2=>Action Of Index

---
+++ Demo.presentation/Controllers/DepartmentsController.cs
@@ -0,0 +1,14 @@
+﻿using Demo.BLL.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.presentation.Controllers
+{
+    public class DepartmentsController(IDepartmentServices _departmentServices) : Controller
+    {
+        public IActionResult Index()
+        {
+            var departments = _departmentServices.GetAllDepartments();
+            return View(departments);
+        }
+    }
+}
