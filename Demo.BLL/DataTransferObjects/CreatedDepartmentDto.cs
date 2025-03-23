5ea73ac9d2c6285c58de726840fc4ebec8da09e9
khaled saied <khaledsaied080@gmail.com>
2025-03-24 01:04:36 +0200

    3=>Action of Create

--- Demo.BLL/DataTransferObjects/CreatedDepartmentDto.cs
+++ Demo.BLL/DataTransferObjects/CreatedDepartmentDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,12 @@ namespace Demo.BLL.DataTransferObjects
 {
     public class CreatedDepartmentDto
     {
+        [Required]
+        [Range(100,int.MaxValue)]
         public string Code { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Invalid Name!!!")]
         public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
 
         public DateOnly DateOfCreation { get; set; }
 
--- Demo.presentation/Controllers/DepartmentsController.cs
+++ Demo.presentation/Controllers/DepartmentsController.cs
@@ -1,14 +1,59 @@
-﻿using Demo.BLL.Services;
+﻿using Demo.BLL.DataTransferObjects;
+using Demo.BLL.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.presentation.Controllers
 {
-    public class DepartmentsController(IDepartmentServices _departmentServices) : Controller
+    public class DepartmentsController(IDepartmentServices _departmentServices,
+        ILogger<DepartmentsController> _logger,
+        IWebHostEnvironment _webHostEnvironment) : Controller
     {
         public IActionResult Index()
         {
             var departments = _departmentServices.GetAllDepartments();
             return View(departments);
         }
+
+        #region Create Department
+        [HttpGet]
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(CreatedDepartmentDto departmentDto)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    int result = _departmentServices.AddDepartment(departmentDto);
+                    if (result > 0)
+                        return RedirectToAction(nameof(Index));
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Department was not created");
+                        //return View(departmentDto);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //log Exception
+                    if (_webHostEnvironment.IsDevelopment())
+                    {
+                        //1- Devolpment=> log error in console and return error message to user
+                        ModelState.AddModelError(string.Empty, ex.Message);
+                        //return View(departmentDto);
+                    }
+                    else
+                    {
+                        //2-Deployment=> log error in file or database and return  error view,
+                        _logger.LogError(ex.Message);
+                        //return View(departmentDto);
+                    }
+                }
+            }
+            return View(departmentDto);
+        } 
+
+        #endregion
     }
 }
