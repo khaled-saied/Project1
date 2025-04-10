ed4e804e442bb996059edc3d597204dcf9258e96
khaled saied <khaledsaied080@gmail.com>
2025-04-10 14:04:16 +0200

    4=>Add Register Action

---
+++ Demo.presentation/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using Demo.DAL.Models.IdentityModel;
+using Demo.presentation.ViewModels.Auth;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.presentation.Controllers
+{
+    public class AccountController(UserManager<ApplicationUser> _userManager) : Controller
+    {
+        #region Register
+        [HttpGet]
+        public IActionResult Register() =>  View();
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid) return View(registerViewModel);
+
+            var User = new ApplicationUser
+            {
+                UserName = registerViewModel.UserName,
+                Email = registerViewModel.Email,
+                FirstName = registerViewModel.FirstName,
+                LastName = registerViewModel.LastName
+            };
+
+            var Result = _userManager.CreateAsync(User, registerViewModel.Password).Result;
+
+            if(Result.Succeeded)
+                return RedirectToAction("Login", "Account");
+            else
+            {
+                foreach (var error in Result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(registerViewModel);
+            }
+        }
+        #endregion
+    }
+}
